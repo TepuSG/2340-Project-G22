d57afc8b2d2bd995ea7db2d14778ba57cd71137c
TepuSG <samgalysh@gmail.com>
2025-10-20 18:26:18 -0400

    Updated UI for Profile and Home Page
    Created a more user friendly user-interface for the profile page. The Home page now has three cards for different links. There will be static images added to these cards in the future.

--- profiles/forms.py
+++ profiles/forms.py
@@ -7,8 +7,50 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['headline', 'skills', 'is_public', 'show_skills', 'show_education', 'show_experience', 'show_links']
+        widgets = {
+            'headline': forms.TextInput(attrs={'class': 'form-control'}),
+            'skills': forms.TextInput(attrs={'class': 'form-control'}),
+            'is_public': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'show_skills': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'show_education': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'show_experience': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'show_links': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
 
 
-EducationFormSet = inlineformset_factory(Profile, ProfileEducation, fields=('school','degree','start_year','end_year'), extra=1, can_delete=True)
-ExperienceFormSet = inlineformset_factory(Profile, ProfileExperience, fields=('company','title','start_year','end_year','description'), extra=1, can_delete=True)
-LinkFormSet = inlineformset_factory(Profile, ProfileLink, fields=('label','url'), extra=1, can_delete=True)
+class ProfileEducationForm(forms.ModelForm):
+    class Meta:
+        model = ProfileEducation
+        fields = ['school', 'degree', 'start_year', 'end_year']
+        widgets = {
+            'school': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'School/University'}),
+            'degree': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Degree/Field of Study'}),
+            'start_year': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Start Year'}),
+            'end_year': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'End Year'}),
+        }
+
+class ProfileExperienceForm(forms.ModelForm):
+    class Meta:
+        model = ProfileExperience
+        fields = ['company', 'title', 'start_year', 'end_year', 'description']
+        widgets = {
+            'company': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Company Name'}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Job Title'}),
+            'start_year': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Start Year'}),
+            'end_year': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'End Year'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Brief description of your role and accomplishments'}),
+        }
+
+class ProfileLinkForm(forms.ModelForm):
+    class Meta:
+        model = ProfileLink
+        fields = ['label', 'url']
+        widgets = {
+            'label': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g., LinkedIn, GitHub, Portfolio'}),
+            'url': forms.URLInput(attrs={'class': 'form-control', 'placeholder': 'https://example.com'}),
+        }
+
+
+EducationFormSet = inlineformset_factory(Profile, ProfileEducation, form=ProfileEducationForm, extra=1, can_delete=True)
+ExperienceFormSet = inlineformset_factory(Profile, ProfileExperience, form=ProfileExperienceForm, extra=1, can_delete=True)
+LinkFormSet = inlineformset_factory(Profile, ProfileLink, form=ProfileLinkForm, extra=1, can_delete=True)
